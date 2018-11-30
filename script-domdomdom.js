d9d026d66eb7e3dd21871b9de36c3e97d25da4cd
jleetaylor2020 <jleetaylor2020@icloud.com>
2018-11-30 15:21:06 -0600

    updating labs

--- script-domdomdom.js
+++ script-domdomdom.js
@@ -6,3 +6,39 @@ document.addEventListener('DOMContentLoaded', function () {
     button.appendChild(btnText);
     document.body.appendChild(button);
     document.body.appendChild(containerDiv);
+
+    let id = 1;
+
+    button.addEventListener('click', function () {
+        let squareDiv = document.createElement('div');
+        let squareDivText = document.createTextNode(id);
+        // squareDiv.append(squareDivText);
+        let thisId = id
+        squareDiv.className = 'square';
+        squareDiv.id = thisId;
+        id++;
+        containerDiv.append(squareDiv);
+
+        squareDiv.addEventListener('mouseover', function () {
+            squareDiv.textContent = thisId;
+        });
+
+        squareDiv.addEventListener('mouseleave', function () {
+            squareDiv.textContent = '';
+        });
+
+        squareDiv.addEventListener('click', function () {
+            squareDiv.style.backgroundColor = getRandomColor();
+        });
+        squareDiv.addEventListener('dblclick', function () {
+            squareDiv.remove();
+        });
+    });
+});
+
+function getRandomColor() {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + "," + g + "," + b + ")";
+};
--- scriptdom99lines.js
+++ scriptdom99lines.js
@@ -23,10 +23,20 @@ document.addEventListener('DOMContentLoaded', function () {
         var list = ['John', 'James', 'Kim', 'Carrie', 'Liz'];
 
         for (var i = 0; i < list.length; i++) {
-            console.log(list[i])
+            // console.log(list[i])
+            let header = document.createElement('h3');
+            let headerText = document.createTextNode(list[i]);
+            header.append(headerText);
+            header.style.color = 'red';
+            div.append(header);
 
             for (j = 99; j > 0; j--) {
-                console.log([j] + ' lines of code on the wall, ' + [j] + ' lines of code. One fell off and made a fall ')
+                // console.log([j] + ' lines of code on the wall, ' + [j] + ' lines of code. One fell off and made a fall ')
+                let p = document.createElement('p');
+                let pText = document.createTextNode([j] + ' lines of code on the wall, ' + [j] + ' lines of code. One fell off and made a fall ')
+                p.append(pText);
+                div.append(p);
+
                 if (j < 2) {
                     console.log([j] + ' line of code remains on the wall!' + [j] + ' line of code. One fell off and made a fall ')
                     console.log([j - 1] + ' line of code remains on the wall!' + [j - 1] + ' lines of code remain! ')
